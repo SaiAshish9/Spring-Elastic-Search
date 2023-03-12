709e8ef91553062a105f5bdb41ed8493e3877a42
SaiAshish <saiashish3760@gmail.com>
2023-03-12 14:52:49 +0530

    Add files via upload

---
+++ src/test/java/com/sai/elk/ElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sai.elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
